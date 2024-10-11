d4a6788e4bfea3105fd0de974030cf0e1f43c0e7
Willian -- Lab02 <edu.cascacity@gmail.com>
2024-10-11 20:31:03 -0300

    Ok

--- introJS.js
+++ introJS.js
@@ -3,25 +3,37 @@
 
 // CLICK NO BOTÃO
 const frm = document.querySelector("form");
-const resp = document.querySelector("h2");
+const resp = document.querySelector("h3");
+const media = document.querySelector("h2");
 
 
 frm.addEventListener("submit", (e) => {
-    
     e.preventDefault(); // PREVENDE O SUBMITE DO FORMULÃ��RIO
-    const nome = frm.nome.value;
-    resp.innerText = ("Olá " + nome + " Seja bem vindo ao nosso site! Gentileza informar suas notas para calcular a média.");
-    const n1 = Number(frm.n1.value);
-    const n2 = Number(frm.n2.value);
-    const n3 = Number(frm.n3.value);
-    const n4 = Number(frm.n4.value);
+    const nome = frm.inNome.value;
+    resp.innerText =`Olá ${nome} Seja bem vindo ao nosso site!`;
 
-    const media = (n1 + n2 + n3 + n4) / 4;
 
-    resp.innerText = `nA média das suas notas é:` ${media};
+    const n1 = parseFloat(frm.n1.value);
+    const n2 = parseFloat(frm.n2.value);
+    const n3 = parseFloat(frm.n3.value);
+    const n4 = parseFloat(frm.n4.value);
 
+    const mediaAluno = ((n1 + n2 + n3 + n4) / 4);
+
+    media.innerText = `A media final é ${mediaAluno.toFixed(2)}`;
+
+    if(mediaAluno>=7) {
+        resp.innerText="Colando até eu - Aprovado"; 
+        media.style.color="Blue";
+    }
+    if(mediaAluno<=6.9 && mediaAluno>=4){
+        resp.innerText="Ta na Bosta - Recuperação";
+        media.style.color="Green";
+    }
+    if(mediaAluno<3.9){
+        resp.innerText="Se fodeu - Reprovado";
+        media.style.color="red;"
+
+    }
     
-
-
-
 });
